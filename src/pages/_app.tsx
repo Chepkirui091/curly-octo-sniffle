32f1810def21b2d130e257a93458448064b2f7ad
daphne <chepkiruidaphne91@gmail.com>
2024-12-18 17:29:10 +0300

    todo pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Sidebar from "@/components/page-components/sidebar";
+import { ThemeProvider } from "@/context/theme-context";
+import TopNav from "@/components/page-components/top-nav";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <ThemeProvider>
+            <div className="flex flex-col h-screen">
+                {/* Top Navbar */}
+                <TopNav />
+
+                {/* Main Content and Sidebar */}
+                <div className="flex flex-1 overflow-hidden">
+                    <Sidebar />
+                    <main className="flex-1 p-6 overflow-auto">
+                        <Component {...pageProps} />
+                    </main>
+                </div>
+            </div>
+        </ThemeProvider>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "media",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    // extend: {
+    //   colors: {
+    //     background: "var(--background)",
+    //     foreground: "var(--foreground)",
+    //   },
+    // },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightBackground: "#f8f9fa",
+        darkBackground: "#1a202c",
       },
     },
   },
